beed5d9911880ff06c57e8eeae395a392b5e442b
roxana.popescu <Bb123456++>
2020-08-31 13:59:56 +0300

    P04 (*) Find the number of elements of a list.

--- Pb4/app/src/main/java/com/example/pb4/MainActivity.kt
+++ Pb4/app/src/main/java/com/example/pb4/MainActivity.kt
@@ -1,26 +1,17 @@
 package com.example.pb4
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-
 /**
  * P04 (*) Find the number of elements of a list.
-* */
-class MainActivity : AppCompatActivity() {
+ * */
+fun <T> List<T>.tail(): List<T> = drop(1)
 
-    fun <T> List<T>.tail(): List<T> = drop(1)
-
-     fun <T> nrOfElements(list: List<T>): Int {
-        return if (list.isEmpty()) 0
-        else 1 + nrOfElements(list.tail())
-     }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        Log.i("pb","= "+nrOfElements(listOf(1,1,2,3,4,7,9,90)))
-    }
+fun <T> nrOfElements(list: List<T>): Int {
+    return if (list.isEmpty()) 0
+    else 1 + nrOfElements(list.tail())
 }
+
+fun main() {
+    println(listOf(1, 1, 2, 3, 4, 7, 9, 90))
+    println(" The nr of elements in list is :" + nrOfElements(listOf(1, 1, 2, 3, 4, 7, 9, 90)))
+}
+
--- Pb4/app/src/test/java/com/example/pb4/P04Test.kt
+++ Pb4/app/src/test/java/com/example/pb4/P04Test.kt
@@ -1,5 +1,6 @@
 package com.example.pb4
 
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -9,9 +10,11 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+
+class P04Test {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `reverse a list`() {
+
+        assertThat(nrOfElements(listOf(1, 1, 2, 3, 5, 8)), equalTo(6))
     }
 }
